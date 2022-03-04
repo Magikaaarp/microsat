39b8b3368908c7e645e9c1e44ed13e0a5ab9d985
Magi_karp <magikaaarp@outlook.com>
2022-03-04 21:27:23 +0800

    move [out] and [expected_out] into /data

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-./microsat.exe data/aim-50-1_6-no-1.cnf >out
-./microsat.exe data/aim-50-6_0-yes1-2cnf >>out
-./microsat.exe data/aim-200-1_6-no-1.cnf >>out
-./microsat.exe data/aim-200-1_6-yes1-3.cnf >>out
-diff out expected_out -s
+./microsat.exe data/aim-50-1_6-no-1.cnf >data/out
+./microsat.exe data/aim-50-6_0-yes1-2cnf >>data/out
+./microsat.exe data/aim-200-1_6-no-1.cnf >>data/out
+./microsat.exe data/aim-200-1_6-yes1-3.cnf >>data/out
+diff data/out data/expected_out -s
