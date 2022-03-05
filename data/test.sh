f44d883c4ee41de52c8404258f1d4606d13d3109
Magi_karp <magikaaarp@outlook.com>
2022-03-05 17:35:37 +0800

    added another test case

--- data/test.sh
+++ data/test.sh
@@ -1,5 +1,6 @@
 g++ ../microsat.cpp -O2 -o microsat
 ./microsat aim-50-1_6-no-1.cnf >out
+./microsat aim-50-1_6-yes1-3.cnf >>out
 ./microsat aim-50-6_0-yes1-2.cnf >>out
 ./microsat aim-200-1_6-no-1.cnf >>out
 ./microsat aim-200-1_6-yes1-3.cnf >>out
